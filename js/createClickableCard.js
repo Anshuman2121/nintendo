3a7e6096d0ea7ae212950f78d56090fe27decdf4
anshuman2121 <anshuman2121@gmail.com>
2023-09-04 01:06:42 +0200

    New images added

--- js/createClickableCard.js
+++ js/createClickableCard.js
@@ -1,24 +1,38 @@
-function createClickableCard(imageName, link, containerId) {
-    const cardContainer = document.getElementById(containerId);
-    const cardLink = document.createElement('a');
-    cardLink.href = '#';
-    cardLink.className = 'card';
-    cardLink.onclick = function () {
-      myfunction(link);
-      return false;
-    };
+function createClickableCard(imageName, hoverImageName, link, containerId) {
+  const cardContainer = document.getElementById(containerId);
+  const cardLink = document.createElement('a');
+  cardLink.href = '#';
+  cardLink.className = 'card';
+  cardLink.onclick = function () {
+    myfunction(link);
+    return false;
+  };
+
+  const image = document.createElement('img');
+  image.src = `images/${imageName}`;
+  image.className = 'card-img-top';
   
-    const image = document.createElement('img');
+  // Set object-fit property to cover
+  image.style.objectFit = 'cover';
+
+  // Add event listeners for mouseover and mouseout
+  cardLink.addEventListener('mouseover', function() {
+    image.src = `images/${hoverImageName}`;
+    image.classList.add('flap'); // Add flap class on mouseover
+  });
+  
+  cardLink.addEventListener('mouseout', function() {
     image.src = `images/${imageName}`;
-    image.className = 'card-img-top';
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-    const cardTitle = document.createElement('h5');
-    cardTitle.className = 'card-title';
-    cardTitle.textContent = imageName.split('.')[0];
-    cardBody.appendChild(cardTitle);
-    cardLink.appendChild(image);
-    cardLink.appendChild(cardBody);
-    cardContainer.appendChild(cardLink);
-  }
-  
+    image.classList.remove('flap'); // Remove flap class on mouseout
+  });
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  const cardTitle = document.createElement('h5');
+  cardTitle.className = 'card-title';
+  cardTitle.textContent = imageName.split('.')[0];
+  cardBody.appendChild(cardTitle);
+  cardLink.appendChild(image);
+  cardLink.appendChild(cardBody);
+  cardContainer.appendChild(cardLink);
+}
--- js/readCSV.js
+++ js/readCSV.js
@@ -1,11 +1,10 @@
 async function readCSV(file, containerId) {
-    const response = await fetch(file);
-    const text = await response.text();
-    const rows = text.split('\n');
-  
-    for (const row of rows) {
-      const [imageName, link] = row.split(', ');
-      createClickableCard(imageName, link, containerId);
-    }
+  const response = await fetch(file);
+  const text = await response.text();
+  const rows = text.split('\n');
+
+  for (const row of rows) {
+    const [imageName, hoverImageName, link] = row.split(', ');
+    createClickableCard(imageName, hoverImageName, link, containerId);
   }
-  
+}
